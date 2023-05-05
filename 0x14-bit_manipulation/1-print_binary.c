555f2b8fe957e530110b04c37d14432491e88a46
root <you@example.com>
2023-05-05 06:35:58 -0700

    ssndlovu

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,13 +7,13 @@
 void print_binary(unsigned long int n)
 {
 	int x, count = 0;
-	unsigned long int curr;
+	unsigned long int current;
 
 	for (x = 63; x >= 0; x--)
 	{
-		curr = n >> i;
+		current = n >> i;
 
-		if (curr & 1)
+		if (current & 1)
 		{
 			_putchar('1');
 			count++;
